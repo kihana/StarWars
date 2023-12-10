8d34d62a534b5ec3043edbe9c0269884d941cbb0
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 12:55:20 +0100

    Added base Vector structure.

---
+++ server/core/include/core/vector.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <cstdint>
+
+namespace server::core {
+
+struct Vector {
+  std::uint64_t x = 0;
+  std::uint64_t y = 0;
+};
+
+} // namespace server::core
+
+server::core::Vector operator+(const server::core::Vector& v1, const server::core::Vector& v2);
---
+++ server/core/vector.cpp
@@ -0,0 +1,7 @@
+﻿#include "vector.h"
+
+namespace server::core {} // namespace server::core
+
+server::core::Vector operator+(const server::core::Vector& v1, const server::core::Vector& v2) {
+  return server::core::Vector{v1.x + v2.x, v1.y + v2.y};
+}
