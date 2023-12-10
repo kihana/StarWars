2e9785309d2077ac2aa0f6f6ba18b6d46cd76293
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 12:56:07 +0100

    Added Movable and Move classes to implement move action.

---
+++ server/utils/include/utils/move.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "core/vector.h"
+
+namespace server::utils {
+
+struct Movable {
+  virtual ~Movable() = default;
+
+  virtual const core::Vector& GetPosition() const = 0;
+  virtual void SetPosition(const core::Vector& position) = 0;
+  virtual const core::Vector& GetVelocity() const = 0;
+};
+
+class Move {
+public:
+  explicit Move(Movable& movable);
+
+  void Execute();
+
+private:
+  Movable& movable_;
+};
+
+} // namespace server::utils
