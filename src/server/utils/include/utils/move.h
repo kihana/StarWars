3f3237cb0fcc5dd16633a2d33a640075694e6f83
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 22:15:57 +0100

    Fixed move and rotate commands.

--- src/server/utils/include/utils/move.h
+++ src/server/utils/include/utils/move.h
@@ -10,6 +10,7 @@ struct Movable {
   virtual core::Vector GetPosition() const = 0;
   virtual void SetPosition(core::Vector position) = 0;
   virtual core::Vector GetVelocity() const = 0;
+  virtual void SetVelocity(core::Vector velocity) = 0;
 };
 
 class Move {
