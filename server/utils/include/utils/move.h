b1e17794dc0f82e0debbcb9001bbdbd11d2280ac
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 13:55:42 +0100

    Fixed Movable interface.

--- server/utils/include/utils/move.h
+++ server/utils/include/utils/move.h
@@ -7,9 +7,9 @@ namespace server::utils {
 struct Movable {
   virtual ~Movable() = default;
 
-  virtual const core::Vector& GetPosition() const = 0;
-  virtual void SetPosition(const core::Vector& position) = 0;
-  virtual const core::Vector& GetVelocity() const = 0;
+  virtual core::Vector GetPosition() const = 0;
+  virtual void SetPosition(core::Vector position) = 0;
+  virtual core::Vector GetVelocity() const = 0;
 };
 
 class Move {
