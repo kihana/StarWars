94f6454015c6459b07e4fe3ff53fd8dd9a4b7348
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 18:20:58 +0100

    Move all sources to src folder.

---
+++ src/server/utils/rotate.cpp
@@ -0,0 +1,18 @@
+#include "rotate.h"
+
+#include <cmath>
+
+namespace server::utils {
+
+Rotate::Rotate(Rotatable& rotatable) : rotatable_(rotatable) {
+}
+
+void Rotate::Execute() {
+  const auto pos = rotatable_.GetPosition();
+  const auto angle = rotatable_.GetAngle();
+  const auto cos_angle = static_cast<std::uint64_t>(std::cos(angle));
+  const auto sin_angle = static_cast<std::uint64_t>(std::sin(angle));
+  rotatable_.SetPosition(core::Vector{pos.x * cos_angle - pos.y * sin_angle, pos.x * sin_angle + pos.y * cos_angle});
+}
+
+} // namespace server::utils
