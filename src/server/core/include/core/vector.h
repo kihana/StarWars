35ac56c8903115d3ccddc6313a632736e0ca4a1d
Egor Zayats <yahor.zayats@atotech.com>
2023-12-10 22:14:59 +0100

    Enhanced interface for vector with == operator.

--- src/server/core/include/core/vector.h
+++ src/server/core/include/core/vector.h
@@ -5,10 +5,14 @@
 namespace server::core {
 
 struct Vector {
-  std::uint64_t x = 0;
-  std::uint64_t y = 0;
+  Vector() = default;
+  Vector(std::int64_t x, std::int64_t y) : x(x), y(y){};
+
+  std::int64_t x = 0;
+  std::int64_t y = 0;
 };
 
 } // namespace server::core
 
-server::core::Vector operator+(const server::core::Vector& v1, const server::core::Vector& v2);
+server::core::Vector operator+(server::core::Vector v1, server::core::Vector v2);
+bool operator==(server::core::Vector v1, server::core::Vector v2);
--- src/server/core/vector.cpp
+++ src/server/core/vector.cpp
@@ -2,6 +2,10 @@
 
 namespace server::core {} // namespace server::core
 
-server::core::Vector operator+(const server::core::Vector& v1, const server::core::Vector& v2) {
+server::core::Vector operator+(const server::core::Vector v1, const server::core::Vector v2) {
   return server::core::Vector{v1.x + v2.x, v1.y + v2.y};
+}
+
+bool operator==(const server::core::Vector v1, const server::core::Vector v2) {
+  return v1.x == v2.x && v1.y == v2.y;
 }
