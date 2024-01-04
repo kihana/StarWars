26b83c910697aefb00dfa6eb1736941f28245612
Egor Zayats <yahor.zayats@atotech.com>
2024-01-04 09:53:39 +0100

    Fixed configure and build scripts.

--- cmake/scripts/build.cmake
+++ cmake/scripts/build.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.8)
 
-include(${CMAKE_SOURCE_DIR}/cmake/scripts/configure.txt)
+include(${CMAKE_SOURCE_DIR}/cmake/scripts/configure.cmake)
 
 message("Build solution.")
 execute_process(COMMAND cmake --build ${CMAKE_SOURCE_DIR}/build --config Release WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build)
