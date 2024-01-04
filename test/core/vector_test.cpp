f77a9b37cc18283518dc8fef48f17ca4928dad27
Egor Zayats <yahor.zayats@atotech.com>
2024-01-04 12:04:32 +0100

    Fixed vector implementation.

--- test/core/vector_test.cpp
+++ test/core/vector_test.cpp
@@ -2,13 +2,13 @@
 
 #include "core/vector.h"
 
-using namespace server::core;
+namespace server::core {
 
-TEST(Vector, EqualsTest) {
+TEST(VectorTest, Equals) {
   Vector v1(-3, 4);
   Vector v2(-3, 4);
   Vector v3(-9, 4);
-  
+
   EXPECT_TRUE(v1 == v2);
   EXPECT_TRUE(v2 == v1);
   EXPECT_FALSE(v1 == v3);
@@ -17,13 +17,28 @@ TEST(Vector, EqualsTest) {
   EXPECT_FALSE(v3 == v2);
 }
 
-TEST(Vector, PlusTest) {
+TEST(VectorTest, NotEquals) {
   Vector v1(-3, 4);
   Vector v2(-3, 4);
   Vector v3(-9, 4);
 
-  EXPECT_TRUE(v1 + v2 == Vector(-6, 8));
-  EXPECT_TRUE(v1 + v3 == Vector(-12, 8));
-  EXPECT_TRUE(v2 + v3 == Vector(-12, 8));
-  EXPECT_FALSE(v3 + v2 == Vector(-13, 9));
+  EXPECT_FALSE(v1 != v2);
+  EXPECT_FALSE(v2 != v1);
+  EXPECT_TRUE(v1 != v3);
+  EXPECT_TRUE(v3 != v1);
+  EXPECT_TRUE(v2 != v3);
+  EXPECT_TRUE(v3 != v2);
 }
+
+TEST(VectorTest, Plus) {
+  Vector v1(-3, 4);
+  Vector v2(-3, 4);
+  Vector v3(-9, 4);
+
+  EXPECT_EQ(v1 + v2, Vector(-6, 8));
+  EXPECT_EQ(v1 + v3, Vector(-12, 8));
+  EXPECT_EQ(v2 + v3, Vector(-12, 8));
+  EXPECT_NE(v3 + v2, Vector(-13, 9));
+}
+
+} // namespace server::core
