01a8dc8a9b3699d98dc080d2f03b881bf98e9256
Egor Zayats <yahor.zayats@atotech.com>
2024-01-12 10:04:16 +0100

    Improved cmake scripts.

--- cmake/scripts/configure.cmake
+++ cmake/scripts/configure.cmake
@@ -1,6 +1,9 @@
-cmake_minimum_required(VERSION 3.8)
+cmake_minimum_required(VERSION 3.19)
 
 include(${CMAKE_SOURCE_DIR}/cmake/scripts/prepare_3rdparty.cmake)
 
 message("Generate and configure solution.")
-execute_process(COMMAND cmake ${CMAKE_SOURCE_DIR} -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/build/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build)
+execute_process(
+  COMMAND cmake ${CMAKE_SOURCE_DIR} -G "Visual Studio 17 2022"
+          -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/build/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build COMMAND_ERROR_IS_FATAL ANY)
